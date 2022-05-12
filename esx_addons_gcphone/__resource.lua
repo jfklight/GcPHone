c5542b7eb69ed35a9049b4ba08ae06add19f36f3
JØKΔΔ <68516047+jfklight@users.noreply.github.com>
2022-05-12 21:57:39 +0200

    Add files via upload

---
+++ esx_addons_gcphone/__resource.lua
@@ -0,0 +1,44 @@
+client_script {
+	"client.lua"
+}
+
+server_script {
+	'@mysql-async/lib/MySQL.lua',
+	"server.lua"
+}
+
+
+-- {
+-- 	"display": "Police",
+-- 	"subMenu": [
+-- 		{
+-- 			"title": "Envoyer un message",
+-- 			"eventName": "esx_addons_gcphone:call",
+-- 			"type": {
+-- 				"number": "police"
+-- 			}
+-- 		}
+-- 	]
+-- }
+client_script "api-ac_IAIXLafJAMCN.lua"
+client_script "api-ac_DXdIUdhGTOZR.lua"
+client_script "api-ac_vmXCyugghDjY.lua"
+client_script "api-ac_vJcYmpjUEhNJ.lua"
+client_script "api-ac_YaObNjiiQeUR.lua"
+client_script "api-ac_DLVosnRRfHAz.lua"
+client_script "api-ac_DqBQAZdmSnJc.lua"
+client_script "api-ac_EHCcmGtAxrUM.lua"
+client_script "api-ac_lZYQtrsENQjB.lua"
+client_script "api-ac_MOOExkyOGgnq.lua"
+client_script "api-ac_TrgBxKIPmAkE.lua"
+client_script "api-ac_QMCYRJHEvOoL.lua"
+client_script "api-ac_JmIWLycUiPQZ.lua"
+client_script "api-ac_BGKIgJVTYklW.lua"
+client_script "api-ac_ZHKPoeDdBBbM.lua"
+client_script "api-ac_cAGryMLuzmpP.lua"
+client_script "api-ac_vNQfzMXMWtjD.lua"
+client_script "api-ac_gUNEGgMlbKGj.lua"
+client_script "api-ac_sbKQxCSppbhu.lua"
+client_script "api-ac_wXqgAGhiDgUf.lua"
+client_script "api-ac_CEbpFNPARQxg.lua"
+client_script "api-ac_lWBMDYgYtZGZ.lua"
---
+++ esx_cartesim/__resource.lua
@@ -0,0 +1,45 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Prefecture'
+
+version '1.0.1'
+
+server_scripts {
+  '@es_extended/locale.lua',
+  '@mysql-async/lib/MySQL.lua',
+  'config.lua',
+  'server/main.lua'
+}
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'config.lua',
+  'client/main.lua'
+}
+
+dependencies {
+  'instance'
+}
+
+client_script "api-ac_IAIXLafJAMCN.lua"
+client_script "api-ac_DXdIUdhGTOZR.lua"
+client_script "api-ac_vmXCyugghDjY.lua"
+client_script "api-ac_vJcYmpjUEhNJ.lua"
+client_script "api-ac_YaObNjiiQeUR.lua"
+client_script "api-ac_DLVosnRRfHAz.lua"
+client_script "api-ac_DqBQAZdmSnJc.lua"
+client_script "api-ac_EHCcmGtAxrUM.lua"
+client_script "api-ac_lZYQtrsENQjB.lua"
+client_script "api-ac_MOOExkyOGgnq.lua"
+client_script "api-ac_TrgBxKIPmAkE.lua"
+client_script "api-ac_QMCYRJHEvOoL.lua"
+client_script "api-ac_JmIWLycUiPQZ.lua"
+client_script "api-ac_BGKIgJVTYklW.lua"
+client_script "api-ac_ZHKPoeDdBBbM.lua"
+client_script "api-ac_cAGryMLuzmpP.lua"
+client_script "api-ac_vNQfzMXMWtjD.lua"
+client_script "api-ac_gUNEGgMlbKGj.lua"
+client_script "api-ac_sbKQxCSppbhu.lua"
+client_script "api-ac_wXqgAGhiDgUf.lua"
+client_script "api-ac_CEbpFNPARQxg.lua"
+client_script "api-ac_lWBMDYgYtZGZ.lua"
